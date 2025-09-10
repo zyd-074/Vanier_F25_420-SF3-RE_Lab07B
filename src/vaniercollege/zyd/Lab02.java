fa6884969901b2b05e480dca496bb134c7590503
Z.Y.D. | "Cloumy <2480549@edu.vaniercollege.qc.ca>
2025-09-10 11:01:58 -0400

    Loading a Random Image

--- src/vaniercollege/zyd/Lab02.java
+++ src/vaniercollege/zyd/Lab02.java
@@ -1,11 +1,12 @@
 package vaniercollege.zyd;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.image.*;
+import java.util.Random;
 
 /**
  * @author Yu Duo Zhang
@@ -21,23 +22,32 @@ public class Lab02 extends Application{
 
     @Override
     public void start(Stage stage) {
+        // Building Layout
         BorderPane root = new BorderPane();
         StackPane middle = new StackPane();
         
+        // Object Creation
         Label title = new Label("Random Game");
         Label bottom = new Label("Waiting...");
         Label lblImage = new Label();
+
+        // Random Image impliment
+        int imageNum = new Random().nextInt(20) + 101;
+        String imagePath = String.format("file:images/%d.jpg", imageNum);
+        Image img = new Image(imagePath);
+        lblImage.setGraphic(new ImageView(img));
         
-        
+        // Layout Setup
         root.setTop(title);
         root.setBottom(bottom);
         middle.getChildren().add(lblImage);
         root.setCenter(middle);
         
+        // Build Scene
         Scene scene = new Scene(root, 250, 300);
         stage.setScene(scene);
         stage.setTitle("Java Games");
-        stage.show();
+        stage.show(); 
     }
     
 }
