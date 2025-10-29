b88424a92e7dc078b996fba4c7502a74c3f11c2f
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-29 10:09:49 -0400

    chore: Rename File to Lab07B & Modify Objects

--- src/vaniercollege/zyd/Lab07B_YuDuoZhang.java
+++ src/vaniercollege/zyd/Lab07B_YuDuoZhang.java
@@ -5,15 +5,14 @@ import javafx.scene.Scene;
 import javafx.stage.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.image.*;
-import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * @author Yu Duo Zhang
- * 2025/09/10
- * https://github.com/Cloumy074/Lab02-F25.git
+ * https://github.com/zyd-074/Vanier_F25_420-SF3-RE_Lab07B.git
  */
-public class Lab02_YuDuoZhang extends Application{
+public class Lab07B_YuDuoZhang extends Application{
 
     /**
      * @param args the command line arguments
@@ -27,23 +26,20 @@ public class Lab02_YuDuoZhang extends Application{
         // Building Layout
         BorderPane root = new BorderPane();
         StackPane middle = new StackPane();
+        HBox btns = new HBox();
         
         // Object Creation
-        Label title = new Label("Random Game");
-        Label bottom = new Label("Waiting...");
-        Label lblImage = new Label();
-
-        // Random Image impliment
-        int imageNum = new Random().nextInt(20) + 101;
-        String imagePath = String.format("file:images/%d.jpg", imageNum);
-        Image img = new Image(imagePath);
-        lblImage.setGraphic(new ImageView(img));
+        Label title = new Label();
+        Image[] images = new Image[20];
+        ImageView imgView = new ImageView();
+        Button play = new Button();
+        Button speedPlus = new Button("Speed+");
+        Button speedMinus = new Button("Speed-");
         
         // Layout Setup
-        root.setTop(title);
-        root.setBottom(bottom);
-        middle.getChildren().add(lblImage);
+        btns.getChildren().addAll(speedMinus, play, speedPlus);
         root.setCenter(middle);
+        root.setBottom(btns);
         
         // Build Scene
         Scene scene = new Scene(root, 250, 300);
@@ -51,5 +47,4 @@ public class Lab02_YuDuoZhang extends Application{
         stage.setTitle("Java Games");
         stage.show(); 
     }
-    
 }
